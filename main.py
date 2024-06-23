5452675443dfa9aaadf6716caf26e539e5301903
Ausuar <2156429946@qq.com>
2024-06-23 13:48:35 +0800

     init commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+from pydantic import BaseModel
+
+app = FastAPI()
+DB = {}
+
+class DictItem(BaseModel):
+    key: str
+    value: str
+
+
+@app.get("/items", response_model=DictItem)
+def get_key(key: str):
+    value = DB.get(key,'') 
+    return DictItem(key=key, value=value)
+
+
+
+@app.post("/items",response_model=DictItem)
+def set_key(body: DictItem):
+   DB[body.key]=body.value
+   return DictItem(key=body.key,value=body.value)
+
