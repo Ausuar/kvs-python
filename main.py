bac0eb624f0b377d1124084553231d67911fc4b2
Ausuar <2156429946@qq.com>
2024-06-23 14:38:51 +0800

    洗吧

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 
 from pydantic import BaseModel
 
@@ -8,12 +8,13 @@ DB = {}
 class DictItem(BaseModel):
     key: str
     value: str
-    lkl: str | None
 
 
 @app.get("/items", response_model=DictItem)
 def get_key(key: str):
-    value = DB.get(key,'') 
+    value = DB.get(key)
+    if value is None:
+        raise HTTPException(status_code=404) 
     return DictItem(key=key, value=value)
 
 
@@ -23,3 +24,10 @@ def set_key(body: DictItem):
    DB[body.key]=body.value
    return DictItem(key=body.key,value=body.value)
 
+@app.delete("/itmes/{key}", response_model=DictItem)
+def delete_key(key):
+    if key in DB:
+        value=DB.pop(key)
+    else:
+        raise HTTPException(status_code=404,detail="key is not found")
+    return DictItem(key=key, value=value)
